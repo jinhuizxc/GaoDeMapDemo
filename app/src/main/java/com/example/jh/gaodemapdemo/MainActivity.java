7b562b929a95e0506eee13a30e01d1c205dc4dfc
JinHui <1004260403@qq.com>
2017-10-17 18:16:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/gaodemapdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.jh.gaodemapdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 根据高德地图API 写的demo，
+ * 学习内容：
+ * AMap_2DMap_V2.9.0_20160525
+ * AMap_Location_V2.6.0_20160628
+ * AMap_Search_V3.3.0_20160616
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
